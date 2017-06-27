15a08f1fde92f469eafe8dcd7d3a84e0a299d307
weiyik <weiyi1994@sjtu.edu.cn>
2017-06-27 16:59:42 +0800

    weried issue

--- src/main/java/distrubutelock/LeaderServerEngine.java
+++ src/main/java/distrubutelock/LeaderServerEngine.java
@@ -1,26 +1,17 @@
 package distrubutelock;
-/** 
-* @author xfhuang 
-* @email lanecatm@sjtu.edu.cn
-* @date 2017年6月27日 下午4:25:12
-* @version 
-* Introduction
-*/
+
 public class LeaderServerEngine implements IServerEngine {
 
-	@Override
 	public String handleTryLock(String clientId, String lockKey) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String handleTryRelease(String clientId, String lockKey) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String handleCheck(String clientId, String lockKey) {
 		// TODO Auto-generated method stub
 		return null;
